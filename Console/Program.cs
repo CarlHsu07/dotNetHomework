08f8a0274b4371554867a08f58cfec651a2866be
User <User@P215-2203-NB01>
2022-12-26 08:58:35 +0800

    加入專案檔案。

---
+++ Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notNet_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(MultiplicationTableUtility.CalcMultiplication(2, 9, 9));
+        }
+
+
+    }
+
+    public class MultiplicationTableUtility
+    {
+        public static string CalcMultiplication(int MultiplicandStart, int MultiplicandEnd, int Multiplier)
+        {
+            if (MultiplicandStart > MultiplicandEnd) {
+                int temp;
+                temp = MultiplicandStart;
+                MultiplicandStart = MultiplicandEnd;
+                MultiplicandEnd = temp;
+            }
+            if (MultiplicandStart < 2 || MultiplicandEnd > 9) { 
+                MultiplicandStart = 2;
+                MultiplicandEnd = 9;
+            }
+            if (Multiplier < 1 || Multiplier > 9) { Multiplier = 9; }
+
+            string result = string.Empty;
+            for (int multiplicand = MultiplicandStart; multiplicand <= MultiplicandEnd; multiplicand++)
+            {
+                for (int multiplier = 1; multiplier <= Multiplier; multiplier++)
+                {
+                    result += $"{multiplicand} * {multiplier} = {(multiplicand * multiplier).ToString().PadLeft(2)}\n";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
+    }
+
+
+}
---
+++ dotNet_Console_TaiwanStockTradingTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_Console_TaiwanStockTradingHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 12, 25, 13, 30, 00);
+            Console.WriteLine($"{dt}\n{IsTradingHours(dt)}");
+            
+        }
+
+        public static bool IsTradingHours(DateTime dt)
+        {
+            TimeSpan ts1 = new TimeSpan(9, 00, 00);
+            TimeSpan ts2 = new TimeSpan(13, 30, 00);
+
+            if ((dt.DayOfWeek != DayOfWeek.Saturday) && (dt.DayOfWeek != DayOfWeek.Sunday)) {
+                if (dt.TimeOfDay >= ts1 && dt.TimeOfDay <= ts2) { return true; }
+            }
+            return false;
+        }
+    }
+}
